72d5429808ea440c17c4df177ece369f21a6ec5f
mbarrientos.webdevelopment@gmail.com <91514897+mbarrientos1129@users.noreply.github.com>
2022-04-28 22:49:58 -0400

    Fixed files

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const inquirer = require("inquirer");
-const connection = require ("./connection")
 
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your username
+    user: "",
+    // Your password
+    password: "",
+    database: "employee_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
 function mainMenu() {
     inquirer.prompt([
         {
